9dd71c31fb54e785f52e8ca38bef9887c990abcc
Nayton Batista <naytonbatista@gmail.com>
2018-11-28 14:50:41 -0200

    Inicio aplicação electron

---
+++ app/index.js
@@ -0,0 +1,35 @@
+const { BrowserWindow, app } = require('electron');
+const path = require('path');
+const url = require('url');
+const { exec} = require('child_process');
+
+
+let mainWindow;
+
+
+exec(path.join(__dirname, 'start_server.bat'), (err, stdout, stderr)=> {
+    if (err) {
+        console.error(err);
+        return;
+      }
+      console.log(stdout);
+})
+
+function createWindow() {
+
+    let mainWindow = new BrowserWindow({ 
+        width: 800, 
+        height: 600 ,
+        title:'Pic Tag App'
+    });
+
+    let file = url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file',
+                
+    });
+
+    mainWindow.loadURL(file);
+}
+
+app.on('ready', createWindow);
---
+++ app/start_server.bat
@@ -0,0 +1,1 @@
+cd C:\Users\naytonbatista\Documents\pic-tag\backend && npm start
